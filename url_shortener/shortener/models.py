1163fa0f5f067f404da4eb8a0094f5c4a3486ee1
pawelqq1 <pawelqq1@gmail.com>
2025-06-11 19:45:07 +0200

    adjusted max_length

--- url_shortener/shortener/models.py
+++ url_shortener/shortener/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ShortURL(models.Model):
     long_url = models.URLField()
-    shortcode = models.CharField(max_length=6, unique=True)
+    shortcode = models.CharField(max_length=5, unique=True)
